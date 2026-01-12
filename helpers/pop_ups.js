e9501ba3f7d4d48592d8e60a0691da3c9b3ce174
AngealaBordeniuc <angelabordeniuc5@gmail.com>
2026-01-12 16:44:46 +0200

    Client vulnerability

--- helpers/pop_ups.js
+++ helpers/pop_ups.js
@@ -1,66 +1,36 @@
 import { expect } from '@playwright/test'
 
-export const  handleOptionalPopups = async(page) => {
-  try {
-    await handleCookiesPopUp(page);
-  } catch (e) {
-    // dacă nu există popup, ignorăm
-  }
-
-  try {
-    await handleStayOnSitePopUp(page);
-  } catch (e) {
-    // dacă nu există popup, ignorăm
-  }
-  try {
-    await acceptAllCookies(page); // popup cookies
-  } catch (e) {
-    // dacă nu există, ignorăm
-  }
-
-}
+export const handleOptionalPopups = async (page) => { 
+  await handleCookiesPopUp(page);
+  await handleStayOnSitePopUp(page);
+  await acceptAllCookies(page);
+};
 
 export const handleCookiesPopUp = async (page) => {
   if (page.isClosed()) return;
 
   const closeBtn = page.locator('button[aria-label="Close"]');
 
-  if ((await closeBtn.count()) === 0) return;
-
-  await closeBtn.click();
-  // await closeBtn.waitFor({ state: "detached", timeout: 1000 });
-
-  // // ❗ Așteaptă un pic ca pagina să fie stabilă
-  // await page.waitForTimeout(300); // 300ms sau 500ms
+  try {
+    await closeBtn.waitFor({ state: "visible", timeout: 3000 });
+    await closeBtn.click();
+  } catch {
+    return; 
+  }
 };
-
-
-
 
 export const handleStayOnSitePopUp = async (page) => {
   if (page.isClosed()) return;
 
   const stayBtn = page.getByText("Stay on this site");
 
-  if ((await stayBtn.count()) === 0) return;
-
-  await stayBtn.click();
-  await stayBtn.waitFor({ state: "detached", timeout: 1000 });
+  try {
+    await stayBtn.waitFor({ state: "visible", timeout: 3000 });
+    await stayBtn.click();
+  } catch {
+    return; 
+  }
 };
-
-
-
-export const handleModalWindowSignUp = async (page) => {
-  if (page.isClosed()) return;
-
-  const modal = page.locator('[data-sentry-component="Modal"]');
-
-  if ((await modal.count()) === 0) return;  
-  await expect(modal).toBeVisible();
-};
-
-
-
 
 export const acceptAllCookies = async (page) => {
   if (page.isClosed()) return;
@@ -69,13 +39,29 @@ export const acceptAllCookies = async (page) => {
     name: /accept all cookies/i,
   });
 
-  if (await acceptAllBtn.count() === 0) return;
+  try {
+    await acceptAllBtn.waitFor({ state: "visible", timeout: 3000 });
+    await acceptAllBtn.click();
+  } catch {
+    return; 
+  }
+};
 
-  await acceptAllBtn.click();
-  await acceptAllBtn.waitFor({ state: "detached", timeout: 1000 });
+export const handleModalWindowSignUp = async (page) => {
+  if (page.isClosed()) return;
+
+  const modal = page.locator('[data-sentry-component="Modal"]');
+
+  try {
+    await modal.waitFor({ state: "visible", timeout: 5000 });
+    await expect(modal).toBeVisible();
+  } catch {
+    return; 
+  }
 };
 
 
 
 
-   
+
+
