e6ddce62319de6e9fd6908a2c8c5777ead095e08
AngealaBordeniuc <angelabordeniuc5@gmail.com>
2026-01-12 18:32:00 +0200

    Is Capital Com Safe?

--- pages/aboutSection/IsCapitalComSafePage.js
+++ pages/aboutSection/IsCapitalComSafePage.js
@@ -6,11 +6,19 @@ export class IsCapitalComSafePage{
     }
 
     async clickOpenAnAccountButton(){
-    await handleOptionalPopups(this.page)
-       const openAccountBtn = this.page.getByRole("button", { name: "Open an account" });
-       await openAccountBtn.waitFor({ state: "attached", timeout: 40000 });
-       await openAccountBtn.waitFor({ state: "visible", timeout: 40000 });
-         await openAccountBtn.click();
+  
+    const openAccountBtn = this.page.locator("text=/open an account/i").first();
+     await openAccountBtn.waitFor({ state: "visible", timeout: 40000 });
+
+     await Promise.all([
+       this.page.waitForLoadState("domcontentloaded"),
+       openAccountBtn.click({ force: true }),
+     ]);
+    //    const openAccountBtn = this.page.getByRole("button", { name: "Open an account" });
+    //    await openAccountBtn.waitFor({ state: "attached", timeout: 40000 });
+    //    await openAccountBtn.waitFor({ state: "visible", timeout: 40000 });
+    //      await openAccountBtn.click();
+        //   await handleOptionalPopups(this.page);
          await handleModalWindowSignUp(this.page)
     }
 
