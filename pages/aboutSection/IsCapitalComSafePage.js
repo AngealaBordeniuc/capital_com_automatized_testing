29183318ba486e7edb2e598899a294cf83652b7c
AngealaBordeniuc <angelabordeniuc5@gmail.com>
2026-01-12 18:12:46 +0200

    Ia Capital Com Safe?

--- pages/aboutSection/IsCapitalComSafePage.js
+++ pages/aboutSection/IsCapitalComSafePage.js
@@ -6,7 +6,7 @@ export class IsCapitalComSafePage{
     }
 
     async clickOpenAnAccountButton(){
-        await handleOptionalPopups(this.page)
+    await handleOptionalPopups(this.page)
        const openAccountBtn = this.page.getByRole("button", { name: "Open an account" });
        await openAccountBtn.waitFor({ state: "attached", timeout: 40000 });
        await openAccountBtn.waitFor({ state: "visible", timeout: 40000 });
